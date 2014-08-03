bd029bdb74590a3584d9d9641ab71f603e661534
Daniel Babicky <dudebrodudelol@gmail.com>
2014-08-02 18:10:33 -0700

    auth with twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :omniauthable, 
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:twitter] 
     :confirmable
 validates :name, length: { minimum: 4 }
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
@@ -66,4 +66,16 @@ validates :name, length: { minimum: 4 }
             def to_s
          	"#{name}"
 end
+  def twitter
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 end
